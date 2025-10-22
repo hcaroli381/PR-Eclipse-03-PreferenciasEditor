e958962d1737e2b14c811fe1b366108b1bd7b718
hcaroli381 <hcaroli381@g.educaand.es>
2025-10-22 09:12:43 +0200

    Add files via upload

---
+++ src/Ejemplo.java
@@ -0,0 +1,16 @@
+package tema2_eclipse;
+
+public class Ejemplo {
+	public static void main(String[] args) {
+		int x = 5;
+
+		if (x < 10) {
+			System.out.println("Valor menor que 10");
+		}
+		if (x == 5) {
+			System.out.println("x igual a 5");
+		}
+		System.out.println();
+	}
+
+}
